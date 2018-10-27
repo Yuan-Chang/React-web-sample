3face3ded4c60b564f305b32bd643ed2480f64c0
Yuan Chang <yuanchang_chang@intuit.com>
2018-10-27 04:41:01 -0700

    bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
-import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import 'bootstrap/dist/css/bootstrap.css';
+import './custom-bootstrap.scss';
 
 import App from './components/App';
 import reducers from './reducers';
